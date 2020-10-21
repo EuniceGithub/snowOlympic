719191f45f685e0726af5fe7be4ad1ff71659a27
EuniceGithub <1136679275@qq.com>
2020-10-22 01:45:36 +0800

    第四章练习

---
+++ Desktop/GITBASH/EuniceWorks/3-10/3-10.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b,c;
+    
+    puts("请输入两个整数。");
+	printf("整数A："); scanf("%d",&a);
+	printf("整数B："); scanf("%d",&b); 
+	
+	a-b=c;
+	
+	if(-10<=c<=10)
+	   puts("它们的差小于等于10。");
+	else
+	   puts("它们的差大于等于11。"); 
+	    
+	return 0;
+}
